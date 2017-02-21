0143e0d257816682a947fe2d6f32bc4c4c5e06bb
Ahmed Raza <ahmed.raza@square63.com>
2017-02-21 17:44:21 +0500

    Subscribe method for customized forms

--- src/Subscribe.php
+++ src/Subscribe.php
@@ -120,4 +120,86 @@ class Subscribe {
     }
   }
 
+  public function subscribeMe($username, $email, $status){
+    $table = 'subscribe_subscribers';
+    $data = array(
+      'username'=>$username,
+      'email'=>$email,
+      'status'=>$status,
+      'token'=>session_id()
+      );
+    $check = db_select($table, 'subscriber')
+            ->fields('subscriber')
+            ->condition('email', $email, '=')
+            ->condition('status', 1, '=')
+            ->execute()
+            ->fetchAssoc();
+    if ($check) {
+      drupal_set_message(t('@email already exists in subscribers list.', array('@email'=>$email)), 'error');
+      return false;
+    }else{
+      $insert = db_insert($table)
+              ->fields($data)
+              ->execute();
+      return true;
+    }
+  }
+
+  public function confirmationMail($token, $username, $email, $resend, $remove){
+    global $base_url;
+    $mailManager = \Drupal::service('plugin.manager.mail');
+    if ($remove) {
+      $module = 'subscribe';
+      $key = 'subscribe_remove';
+      $to = $email;
+      $params['email'] = $email;
+      $params['name'] = $username;
+      $params['token'] = $token;
+      $params['link'] = $base_url.'/subscribe/remove?token='.$token;
+      $langcode = \Drupal::currentUser()->getPreferredLangcode();
+      $send = true;
+
+      $result = $mailManager->mail($module, $key, $to, $langcode, $params, NULL, $send);
+      if ($result['result'] !== true) {
+        \Drupal::logger('subscribe-logger')->notice($this->t('Failed to send removal email.'));
+      }else{
+        return $send;
+      }
+    }else if($resend){
+      $module = 'subscribe';
+      $key = 'subscribe_resend';
+      $to = $email;
+      $params['email'] = $email;
+      $params['name'] = $username;
+      $params['token'] = $token;
+      $params['link'] = $base_url.'/subscribe/confirm?token='.$token;
+      $langcode = \Drupal::currentUser()->getPreferredLangcode();
+      $send = true;
+
+      $result = $mailManager->mail($module, $key, $to, $langcode, $params, NULL, $send);
+      if ($result['result'] !== true) {
+        \Drupal::logger('subscribe-logger')->notice($this->t('Failed to send duplicate confirmation email.'));
+      }else{
+        return $send;
+      }
+    }else{
+      $module = 'subscribe';
+      $key = 'subscribe_submit';
+      $to = $email;
+      $params['email'] = $email;
+      $params['name'] = $username;
+      $params['token'] = $token;
+      $params['link'] = $base_url.'/subscribe/confirm?token='.$token;
+      $langcode = \Drupal::currentUser()->getPreferredLangcode();
+      $send = true;
+
+      $result = $mailManager->mail($module, $key, $to, $langcode, $params, NULL, $send);
+      if ($result['result'] !== true) {
+        \Drupal::logger('subscribe-logger')->notice($this->t('Failed to send confirmation email.'));
+      }else{
+        return $send;
+      }
+    }
+  }
+
 }
